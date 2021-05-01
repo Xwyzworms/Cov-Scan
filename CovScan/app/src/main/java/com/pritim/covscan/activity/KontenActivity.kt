4e94c866ecf4e15793d88f3d279c63ccb0349132
XwyzWorm <pratamaazmi1@gmail.com>
2021-05-01 09:22:51 +0700

    add implementation for recognizing image,init Model,Preprocess img

--- CovScan/app/src/main/java/com/pritim/covscan/activity/KontenActivity.kt
+++ CovScan/app/src/main/java/com/pritim/covscan/activity/KontenActivity.kt
@@ -1,12 +1,39 @@
 package com.pritim.covscan.activity
 
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.pritim.covscan.R
 
-class KontenActivity : AppCompatActivity() {
+class KontenActivity : AppCompatActivity(), View.OnClickListener {
+    private val mInputSize = 150
+    private val mModelPath = "ctScan.tflite"
+    private lateinit var classifier : Classifier
+    private  val mLabelPath = "labelct.txt"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content_utama)
+        initClassifier()
+        initViews()
+    }
+
+    private fun initClassifier(){
+        classifier = Classifier(assets,mModelPath,mLabelPath,mInputSize)
+    }
+
+    private  fun initViews() {
+        findViewById<ImageView>(R.id.ImgTapDisini).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val bitmap = ((v as ImageView ).drawable as BitmapDrawable).bitmap
+        val res = classifier.recognizeImage(bitmap)
+        runOnUiThread {
+            Toast.makeText(this,res.toString(),Toast.LENGTH_LONG).show()
+        }
     }
 }
